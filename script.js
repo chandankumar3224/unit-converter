f0c48699575583aab1b7a0526435ae2b21eb20a3
Chandan kumar <152954313+chandankumar3224@users.noreply.github.com>
2024-05-08 13:25:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const inputValue = document.getElementById("number");
+const unit = document.getElementById("unit");
+const button = document.getElementById("converter");
+const result = document.getElementById("result");
+
+button.addEventListener('click', function(){
+  let mianResult = 0;
+  let mainOutput = "";
+
+  if (unit.value === "milesToKm" && inputValue.value > 0) {
+    mianResult = inputValue.value * 1.60934;
+    mainOutput = `${inputValue.value} miles = ${mianResult.toFixed(2)} km`;
+  } else if (inputValue.value === 0 || inputValue.value == "") {
+    mainOutput = "1km = 1.60934miles";
+  }
+  else{
+    mianResult = inputValue.value / 1.60934;
+    mainOutput = `${inputValue.value} km = ${mianResult.toFixed(2)} miles`;
+  }
+  result.innerHTML = mainOutput;
+  inputValue.value = "";
+
+})
